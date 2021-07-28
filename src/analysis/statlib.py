94177e1fb5fbfd58946fa24162e1c549a5e42e33
andylitalo <aylitalo@caltech.edu>
2021-07-28 13:03:51 -0700

    added opt to mask & reduce frames for auto thresh

--- src/analysis/statlib.py
+++ src/analysis/statlib.py
@@ -95,7 +95,8 @@ def plot_roc(fpr, tpr, save_path='', ext='.jpg', show_fig=False,
     return ax
     
 
-def proc_stats(vid_path, mask_data, bkgd, end, start=0, every=1):
+def proc_stats(vid_path, mask_data, bkgd, end, start=0, every=1,
+                apply_mask=True, every_multiplier=100):
     """
     Processes statistics of the images in the video.
 
@@ -111,6 +112,13 @@ def proc_stats(vid_path, mask_data, bkgd, end, start=0, every=1):
         first frame to analyze
     every : int, optional
         Only loads every "every" frame (default = 1)
+    apply_mask : bool, optional
+        If True, will apply mask before computing statistics of a frame.
+        Default True.
+    every_multiplier : int, optional
+        If `apply_mask` is `True`, will multiply `every` by this factor
+        to speed up processing time (since applying the mask slows down
+        computation). Default 100.
 
     Returns
     -------
@@ -136,10 +144,14 @@ def proc_stats(vid_path, mask_data, bkgd, end, start=0, every=1):
     cap.set(cv2.CAP_PROP_POS_FRAMES, start)
     f = start
 
-    # masks background
-    row_lo, _, row_hi, _ = mask.get_bbox(mask_data)
-    bkgd = mask.mask_image(bkgd, mask_data['mask'][row_lo:row_hi])
     # loads rows to crop from mask
+    row_lo, _, row_hi, _ = mask.get_bbox(mask_data)
+
+    if apply_mask:
+        # masks background
+        bkgd = mask.mask_image(bkgd, mask_data['mask'][row_lo:row_hi])
+        in_mask = np.ones(mask_data['mask'].astype(bool)[row_lo:row_hi, :].shape, dtype=int)
+        every *= every_multiplier
 
     while(cap.isOpened()):
         # skips every "every" number of frames
@@ -156,18 +168,25 @@ def proc_stats(vid_path, mask_data, bkgd, end, start=0, every=1):
         
         # converts frame to grayscale and masks
         frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        frame = mask.mask_image(frame_gray, mask_data['mask'])
+        frame = frame_gray #mask.mask_image(frame_gray, mask_data['mask'])
         # crops frame
         frame_cropped = frame[row_lo:row_hi, :]
 
         # loads data for statistics
         signed_diff = frame_cropped.astype(int) - bkgd.astype(int)
-        # ensures only evaluates points in the mask
-        in_mask = np.ones(mask_data['mask'].astype(bool)[row_lo:row_hi, :].shape, dtype=int)
-        mean_list += [np.mean(signed_diff[in_mask])]
-        mean_sq_list += [np.mean(signed_diff[in_mask]**2)]
-        stdev_list += [np.std(signed_diff[in_mask])]
-        min_val_list += [np.min(signed_diff[in_mask])]
+
+        if not apply_mask:
+            # ensures only evaluates points in the mask
+            mean_list += [np.mean(signed_diff)]
+            mean_sq_list += [np.mean(signed_diff**2)]
+            stdev_list += [np.std(signed_diff)]
+            min_val_list += [np.min(signed_diff)]
+        else:
+            # in_mask = np.ones(mask_data['mask'].astype(bool)[row_lo:row_hi, :].shape, dtype=int)
+            mean_list += [np.mean(signed_diff[in_mask])]
+            mean_sq_list += [np.mean(signed_diff[in_mask]**2)]
+            stdev_list += [np.std(signed_diff[in_mask])]
+            min_val_list += [np.min(signed_diff[in_mask])]
 
         # increments frame number
         f += 1
