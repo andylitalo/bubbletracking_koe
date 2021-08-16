10c9d93c8f3d8302b4b106c99620976e5f619239
andylitalo <aylitalo@caltech.edu>
2021-08-16 15:30:23 -0700

    init commit

---
+++ analysis/intensity_profiles.py
@@ -0,0 +1,10 @@
+"""
+intensity_profiles.py plots intensity profiles of images of objects
+after background subtraction next to the original image to give a 
+more tangible sense of edge gradients and signal-to-noise ratio.
+
+Author: Andy Ylitalo
+Date: August 16, 2021
+"""
+
+import matplotlib.pyplot as plt 
