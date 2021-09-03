8ab79a5b0fc670ad8c40e0c2376514cf7f334400
andylitalo <aylitalo@caltech.edu>
2021-09-03 14:53:19 -0700

    runs now

--- analysis/compare_outlier_metrics.py
+++ analysis/compare_outlier_metrics.py
@@ -32,6 +32,26 @@ in Quality Control: Statistical Techniques, Edward F. Mykytka, Ph.D., Editor.
 """
 
 ### IMPORTS LIBRARIES
+# adds path to source
+import sys 
+sys.path.append('../src/')
+
+# standard libraries
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+
+# 3rd party libraries 
+import cv2 
+
+# custom libraries 
+import cvimproc.basic as basic 
+import cvimproc.improc as improc 
+import cvimproc.ui as ui
+import cvimproc.mask as mask
+import genl.fn as fn
+import analysis.statlib as stat 
 
 
 ### PARAMETERS
@@ -40,9 +60,14 @@ vid_path = '../input/sd301_co2/20210207_88bar/sd301_co2_15000_001_100_0335_79_04
 # path to save folder
 save_dir = '../output/analysis/compare_outlier_metrics/sd301_co2_40000_001_050_0280_88_04_10/'
 # histogram plots
-n_bins = 100
-n_bins_im = 255
+n_bins = 255
 hist_max = 50
+# analysis params
+tol = 0.1 # requires mean to be close to 0
+# frames to save (based on visual inspection of interesting frames in
+# ANALYSIS\bubble-proc\sd301_co2\20210207_88bar\sd301_co2_15000_001_100_0335_79_100_04_10\hist_pix\below_mean
+frames_to_save = [933, 4147, 4148, 4149, 4150, 4151, 4152, 4153, 4154, 4167, 
+                    8939, 8940, 8941, 8942, 8943, 8944, 8945]
 
 ### DERIVED PARAMETERS ###
 n_frames = basic.count_frames(vid_path)-1
@@ -54,7 +79,7 @@ def parse_args():
     ap = argparse.ArgumentParser(
         description='Plots histograms and saves image residuals' + \
                     ' where objects are detected.')
-    ap.add_argument('-f', '--framerange', nargs=2, default=(0,0),
+    ap.add_argument('-f', '--framerange', nargs=2, default=(0,100),
                     metavar=('frame_start, frame_end'),
                     help='starting and ending frames to save')
     ap.add_argument('-d', '--savediff', default=0, type=int,
@@ -65,6 +90,8 @@ def parse_args():
                     help='Will only save images if True.')
     ap.add_argument('-x', '--ext', default='jpg',
                     help='Extension for saving images (no ".")')
+    ap.add_argument('-n', '--num_frames_for_bkgd', default=1000, type=int,
+                    help='Number of frames to average to compute background.')
     args = vars(ap.parse_args())
 
     frame_start, frame_end = int(args['framerange'][0]), int(args['framerange'][1])
@@ -72,8 +99,10 @@ def parse_args():
     colormap = args['colormap']
     save = args['save']
     ext = args['ext']
-    
-    return frame_start, frame_end, save_diff, colormap, save, ext
+    num_frames_for_bkgd = args['num_frames_for_bkgd']
+
+    return frame_start, frame_end, save_diff, colormap, \
+            save, ext, num_frames_for_bkgd
 
 
 ########################## MAIN ##############################
@@ -83,7 +112,7 @@ def main():
     ### LOADS VIDEO AND PARAMS
     # reads args
     frame_start, frame_end, \
-    save_diff, colormap, save, ext = parse_args()
+    save_diff, colormap, save, ext, num_frames_for_bkgd = parse_args()
 
     # loads mask data
     first_frame, _ = basic.load_frame(vid_path, 0)
@@ -97,13 +126,86 @@ def main():
 
     
     ### IDENTIFIES FRAMES WITHOUT OBJECTS (SYMM DISTR)
+    symm_frame_list = []
     cap = cv2.VideoCapture(vid_path)
     for f in range(frame_start, frame_end):
-        frame = 
-# collects pixel values and computes stats assuming Gaussian noise
+        # loads frame
+        frame = cv2.cvtColor(basic.read_frame(cap, f), cv2.COLOR_BGR2GRAY)
+        # subtracts background, keeping sign 
+        bkg_sub_sgn = frame.astype(int) - bkgd.astype(int)
+        # checks for symmetric distribution (indicates no objs in foreground)
+        if stat.is_symmetric(bkg_sub_sgn):
+            symm_frame_list +=  [bkg_sub_sgn]
+
+    ### ESTIMATES THRESHOLDS BASED ON ANALYSIS METHODS
+    symm_frame_arr = np.asarray(symm_frame_list)
+    mean = np.mean(symm_frame_arr)
+    stdev = np.sqrt(np.mean(np.square(symm_frame_arr) - mean**2))
+    # ensures that the distribution is indeed symmetric about 0
+    assert np.abs(mean) < tol, 'mean of symmetric images too far from zero'
+
+    # method 1: 3 sigma (loose req)
+    th_3sig = int(3*stdev)
+    # method 2: 5 sigma (strict req)
+    th_5sig = int(5*stdev) 
+    # method 3: IQR (more empirical, extreme version using 3*IQR)
+    q1 = np.percentile(symm_frame_arr, 25)
+    q3 = np.percentile(symm_frame_arr, 75)
+    iqr = q3 - q1
+    th_iqr = int(max(-q1 + 3*iqr, q3 + 3*iqr)) # takes max of 2 metrics
+    ### MODIFIED Z-SCORE
+    # sets a threshold for modified Z-score [2] as 3 or 5,
+    # then solves for corresponding value (x_i in the formula)
+    # method 4: modified z-score > 3
+    median = np.median(symm_frame_arr)
+    # median absolute deviation
+    mad = np.median(np.abs(symm_frame_arr - median))
+    th_mz3 = int(3 * (mad/0.6475 + median))
+    # method 5: modified z-score > 5
+    th_mz5 = int(5 * (mad/0.6475 + median))
+
+    # prints results:
+    print('mu = {0:f}, stdev = {1:f}, q1 = {2:f}, q3 = {3:f}'.format(mean, stdev, q1, q3))
+
+    # collects thresholds and their names
+    th_dict = {'3sig' : th_3sig, '5sig' : th_5sig, 'iqr' : th_iqr,
+                'mz3' : th_mz3, 'mz5' : th_mz5}
+
+    
+    ### PLOTS HISTOGRAMS OF PIXEL VALUES IN SELECTED FRAMES ALONGSIDE THRESHOLDS
+
+    # prepares output directory
+    fn.makedirs_safe(save_dir)
+
+    # loops through and saves desired frames
+    for f in frames_to_save:
+        frame = cv2.cvtColor(basic.read_frame(cap, f), cv2.COLOR_BGR2GRAY)
+        # subtracts background, keeping sign 
+        bkg_sub_sgn = frame.astype(int) - bkgd.astype(int)
+
+        ### Plots histogram
+        # creates figure
+        plt.figure()
+        _ = plt.hist(bkg_sub_sgn.flatten(), n_bins, histtype='step')
+        # plots fitted Gaussian
+        x_gauss = np.linspace(np.min(bkg_sub_sgn), np.max(bkg_sub_sgn), 1000)
+        # computes height of gaussian at 0 by counting 0-valued pixels
+        amplitude = len(np.where(bkg_sub_sgn.flatten() == 0)[0])
+        y_gauss = amplitude * np.exp( -(x_gauss - mean)**2 / (2*stdev**2))
+        plt.plot(x_gauss, y_gauss, 'k--', lw=2, label='gaussian fit')
+        # plots thresholds
+        for name, th in th_dict.items():
+            plt.plot([-th, -th], [0, hist_max], '--', lw=2, label='{0:s} = {1:d}'.format(name, th))
+        plt.title('Frame {0:d}, stdev = {1:.3f}'.format(f, stdev))
+        # zooms in on the bottom of the distribution
+        plt.ylim([0, hist_max])
+        plt.legend()
+        # saves figure
+        if save:
+            plt.savefig(os.path.join(save_dir, '{0:d}.{1:s}'.format(f, ext)))
+
+    return
 
 
-### ESTIMATES THRESHOLDS BASED ON ANALYSIS METHODS
-
-
-### PLOTS HISTOGRAMS OF PIXEL VALUES IN SELECTED FRAMES ALONGSIDE THRESHOLDS
+if __name__ == '__main__':
+    main()
